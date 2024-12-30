7654efdc45e09caa4407ef74b080179eaff2b5fc
Ackvat <toprak_karakaya@hotmail.com>
2024-12-31 01:09:53 +0300

    testing servo kit

---
+++ lib/ackmetton.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from colorama import Fore, Style
+
+def GetColor(color="WHITE"):
+    try:
+        # Dynamically get the color attribute
+        colorCode = getattr(Fore, color.upper(), Fore.RESET)  # Defaults to Fore.RESET if color is invalid
+    except AttributeError:
+        colorCode = Fore.RESET
+
+    return colorCode
+
+def Print(text="PRINT", color="WHITE", padding=False):
+    if padding: pad = "\n"
+    else: pad = ""
+
+    print(Style.BRIGHT + f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}' + Style.RESET_ALL + ' | ' + GetColor(color=color) + text + pad + Style.RESET_ALL)
+
+def Debug(text="DEBUG", color="WHITE", padding=False):
+    if padding: pad = "\n"
+    else: pad = ""
+
+    print(Style.BRIGHT + f'{"["}{Fore.GREEN + "DEBUG"}{Fore.RESET + "] "}' + Style.BRIGHT + f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}' + Style.RESET_ALL + ' | ' + GetColor(color=color) + text + pad + Style.RESET_ALL)
+
+def Verbose(text="VERBOSE", color="WHITE", padding=False):
+    if padding: pad = "\n"
+    else: pad = ""
+
+    print(Style.BRIGHT + f'{"["}{Fore.BLUE + "VERBOSE"}{Fore.RESET + "] "}' + Style.BRIGHT + f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}' + Style.RESET_ALL + ' | ' + GetColor(color=color) + text + pad + Style.RESET_ALL)
+
+def Warn(text="WARN", color="YELLOW", padding=False):
+    if padding: pad = "\n"
+    else: pad = ""
+
+    print(Style.BRIGHT + f'{"["}{Fore.YELLOW + "UYARI"}{Fore.RESET + "] "}' + Style.BRIGHT + f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}' + Style.RESET_ALL + ' | ' + GetColor(color=color) + text + pad + Style.RESET_ALL)
+
+def Error(text="ERROR", color="RED", padding=False):
+    if padding: pad = "\n"
+    else: pad = ""
+
+    print(Style.BRIGHT + f'{"["}{Fore.RED + "HATA"}{Fore.RESET + "] "}' + Style.BRIGHT + f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}' + Style.RESET_ALL + ' | ' + GetColor(color=color) + text + pad + Style.RESET_ALL)
--- test.py
+++ test.py
@@ -1,18 +1,27 @@
+import os
 import time
+
+import lib.ackmetton as ack
+
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 kit.address = 0x40
 kit.frequency = 333
 
-kit.servo[0].actuation_range = 180
-kit.servo[0].set_pulse_width_range(500, 3500)
+kit.servo[0].actuation_range = 190
+kit.servo[0].set_pulse_width_range(500, 2600)
 
-try:
-    while True:
-        kit.servo[0].angle = 0
-        time.sleep(3)
-        kit.servo[0].angle = 180
-        time.sleep(3)
-except KeyboardInterrupt:
-    pass
+Trial = True
+
+if Trial == False:
+    try:
+        while True:
+            kit.servo[0].angle = 0
+            time.sleep(3)
+            kit.servo[0].angle = 180
+            time.sleep(3)
+    except KeyboardInterrupt:
+        pass
+else:
+    kit.servo[0].angle = 190
